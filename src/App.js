2d410ac355a4a23093540eeab741cfd376b20a43
Joel Velis <joelvelis94@gmail.com>
2019-01-25 19:48:50 -0500

    app startup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import { RoomList } from './components/RoomList.js'
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC6mRakDzOBLRGt0tFCjf4i6uwb2j1xWKg",
+    authDomain: "bloc-chat94.firebaseapp.com",
+    databaseURL: "https://bloc-chat94.firebaseio.com",
+    projectId: "bloc-chat94",
+    storageBucket: "bloc-chat94.appspot.com",
+    messagingSenderId: "33874571017"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <RoomList />
     );
   }
 }
---
+++ src/components/RoomList.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+
+export class RoomList extends Component {
+	constructor (props) {
+		super (props); 
+			this.state = { rooms:[] };
+			this.roomsRef = this.props.firebase.database().ref("rooms");
+
+	}
+
+	componentDidMount() {
+     this.roomsRef.on('child_added', snapshot => {
+       const room = snapshot.val();
+       room.key = snapshot.key;
+       this.setState({ rooms: this.state.rooms.concat( room ) })
+     });
+   }
+
+
+	render() {
+		return(
+			<section>
+				<ul>{this.state.rooms.map(room =>
+					<li> return room </li> )}
+				</ul>
+
+			</section>
+
+		);
+	}
+}
+
+export default RoomList;
