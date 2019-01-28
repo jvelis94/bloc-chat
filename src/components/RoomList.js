215ac5eacb3196573a0b618224a146ef832fb42e
Joel Velis <joelvelis94@gmail.com>
2019-01-28 13:56:25 -0500

    create rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,29 +1,56 @@
 import React, {Component} from 'react';
+import './RoomList.css';
 
 
 export class RoomList extends Component {
 	constructor (props) {
 		super (props); 
-			this.state = { rooms:[] };
-			this.roomsRef = this.props.firebase.database().ref("_rooms");
+			this.state = { 
+				rooms:[],
+				newRoomName: '' 
+			};
 
-	}
+			this.roomsRef = this.props.firebase.database().ref("_rooms");	 
+		}
 
 	componentDidMount() {
-     this.roomsRef.on('child_added', snapshot => {
-       const room = snapshot.val();
-       room.key = snapshot.key;
-       this.setState({ rooms: this.state.rooms.concat([room]) })
+    this.roomsRef.on('child_added', snapshot  => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat(room) })
      });
+   }
+
+   createRoom(newRoomName) {
+   	this.roomsRef.push({name: newRoomName});
+   	this.setState({newRoomName: ""});
+   }
+
+   handleChange(e) {
+    this.setState({newRoomName: e.target.value });
+  }
+
+   handleSubmit(e) {
+   	e.preventDefault();
+   	this.createRoom(this.state.newRoomName);
    }
 
 
 	render() {
 		return(
-				<ul>{this.state.rooms.map(room =>
+			<section>
+				<section id='addroom'>
+					<form onSubmit={ (e) => {this.handleSubmit(e)}}>
+						<input type='text' placeholder='Desired Room Name' value={ this.state.newRoomName } onChange={ this.handleChange.bind(this) }/>
+						<input type='submit' value='Add Room'/>
+					</form>
+				</section>
+				<section id='chatroom'>
+					{this.state.rooms.map(room =>
 					<li key={room.key}>{room.name}</li> )}
-				</ul>
-
+				</section>
+			</section>
+			
 		);
 	}
 }
